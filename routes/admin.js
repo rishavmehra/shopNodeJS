ad5391daf317d91edd082a62aed8f106eff97d07
rishavmehra <rishavmehra61@gmail.com>
2023-01-30 22:39:52 +0530

    Dynamic  Routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,19 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const productControllers = require('../controllers/products')
+const adminControllers = require('../controllers/admin')
 
 
-
+ 
 // /admin/add-product => GET
-router.get('/add-product', productControllers.getAddProduct );
+router.get('/add-product', adminControllers.getAddProduct );
+
+// /admin/products => GET
+router.get('/products', adminControllers.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productControllers.postAddProducts);
+router.post('/add-product', adminControllers.postAddProducts);
 
+router.get('/edit-product/:productId', adminControllers.getEditProduct);
 
 module.exports = router
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,17 @@ const express = require('express');
 
 const router = express.Router();
 
-const productControllers = require('../controllers/products')
+const shopControllers = require('../controllers/shop')
 
-router.get('/', productControllers.getProduct);
+router.get('/', shopControllers.getIndex);
+
+router.get('/products', shopControllers.getProducts);
+router.get('/products/:productId', shopControllers.getProduct)
+
+router.get('/cart', shopControllers.getCart);
+
+router.post('/cart', shopControllers.postCart);
+router.get('/orders', shopControllers.getOrders);
+router.get('/checkout', shopControllers.getCheckout);
 
 module.exports = router;
